5b704db0010756ac33147852c34029aaee064b06
akis <flaflabell@gmail.com>
2022-03-31 18:58:23 +0900

    covitSliceを作成

---
+++ src/features/covid/covidSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { RootState } from "../../app/store";
+import dataDaily from "./apiDataDaily.json";
+const apiUrl = "https://api.covid19api.com/total/country";
+
+// レスポンスの型
+type DATADAILY = typeof dataDaily;
+
+// 初期値の型
+type covidState = { 
+  daily: DATADAILY;
+  country: string;
+};
+
+const initialState: covidState = {
+  daily: dataDaily, // 型としてインポートしたデータをそのまま利用する
+  country: "Japan",
+};
+
+export const fetchAsyncGetDaily = createAsyncThunk(
+  "covid/getDaily",
+  async (country: string) => {
+    const { data } = await axios.get<DATADAILY>(`${apiUrl}/${country}`);
+    return { data: data, country: country };
+  }
+);
+
+const covidSlice = createSlice({
+  name: "covid",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAsyncGetDaily.fulfilled, (state, action) => {
+      return {
+        ...state,
+        daily: action.payload.data,
+        country: action.payload.country,
+      };
+    });
+  },
+});
+
+export const selectDaily = (state: RootState) => state.covid.daily;
+export const selectCountry = (state: RootState) => state.covid.country;
+
+export default covidSlice.reducer;
